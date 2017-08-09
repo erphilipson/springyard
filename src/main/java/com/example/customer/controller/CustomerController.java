3930e4e3e3476d93ef5662c0969d4eb2cbb20f58
Ethan Philipson <erphilipson@gmail.com>
2017-08-09 19:18:41 -0400

    Add single customer pages

--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,6 +52,14 @@ public class CustomerController {
     @GetMapping("/admin")
     String admin() {
         return "super_secret";
+    }
+
+    @GetMapping("/customers/{id}")
+    String getCustomer(@PathVariable("id") Integer id, Model model) {
+        Customer customer = customerService.getById(id);
+        System.out.println(customer.getId());
+        model.addAttribute("customer", customer);
+        return "single_customer";
     }
 
     private void showPrinciple() {
